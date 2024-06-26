436aa82d1ae77a5be343948fbba57403719020e4
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-25 18:28:31 -0600

    server now reads from .env

--- userServer/userServer.js
+++ userServer/userServer.js
@@ -1,10 +1,25 @@
 import express from 'express';
 const app = express();
-import { requestToken,  accessToken , input} from './functions.js'
+import { requestToken, accessToken, input } from './functions.js'
 import { createInterface } from 'readline';
-//import { question, close } from 'readline';
-import dotenv from 'dotenv';
 import OAuth from 'oauth-1.0a';
+import path from 'path'; // Import path module for directory handling
+import { createRequire } from 'module';
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// // Use import.meta.url to get the current module's URL
+// const __filename = fileURLToPath(import.meta.url);
+// // Use dirname to get the directory name
+// const __dirname = dirname(__filename);
+
+// // Load environment variables from .env file
+// dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// console.log('Environment variables:');
+// console.log(process.env.CONSUMER_KEY);
+// console.log(process.env.CONSUMER_SECRET);
 
 let oAuthAccessToken
 let oAuthRequestToken
@@ -13,26 +28,44 @@ const requestTokenURL = 'https://api.twitter.com/oauth/request_token?oauth_callb
 const authorizeURL = new URL('https://api.twitter.com/oauth/authorize');
 const accessTokenURL = 'https://api.twitter.com/oauth/access_token';
 
-const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-dotenv.config();
+// dotenv.config('../');
 
 const consumer_key = process.env.CONSUMER_KEY;
 const consumer_secret = process.env.CONSUMER_SECRET;
 
+console.log(consumer_key);
+console.log(consumer_secret)
+
 const oauth = OAuth({
   consumer: {
-      key: consumer_key,
-      secret: consumer_secret
+    key: consumer_key,
+    secret: consumer_secret
   },
   signature_method: 'HMAC-SHA1',
   hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
 });
 
+// app.post('/post', async (req, res) => {
+//   try {
+//     const data = req.data;
+//     console.log(data);
+//     const response = await getRequest(oAuthAccessToken, data.text);
+//     console.dir(response, {
+//       depth: null
+//     });
+//     return response;
 
+//   } catch (error) {
+//     console.log("Error: ", error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+//   process.exit();
+// });
 
 app.listen(3000, async () => {
   console.log('Server listening on port 6969');
